eaacd0486314c3098a8a74aa768332de42c5c145
thunesti <thunestian@gmail.com>
2026-05-20 15:12:44 +0200

    feat: login page

--- app/layouts/blank.vue
+++ app/layouts/blank.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
+  <div class="blank-layout">
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+  .blank-layout {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;    /* Legger header og main under hverandre */
+      justify-content: center;   /* Sentrerer innholdet vertikalt (opp/ned) */
+      align-items: center;       /* Sentrerer BÅDE logo-headeren og main horisontalt (høyre/venstre) */
+      
+      gap: 24px;                 /* Gir et perfekt, fast mellomrom mellom logoen og kortet */
+      padding: 20px;
+      box-sizing: border-box;
+  }
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,15 +5,65 @@
 </script>
 
 <template>
-	<div class="logo-wrapper">
+	<header class="logo-wrapper">
 		<MiniTrelloLogo />
-	</div>
+	</header>
+
+	<main class="login-box">
+		<form class="login-form">
+			<h2 class="form-title">Logg inn på terminalen</h2>
+			
+			<div class="input-group">
+				<label>Brukernavn</label>
+				<input type="text" placeholder="Ditt brukernavn..." />
+			</div>
+			
+			<button type="submit">Start økt</button>
+		</form>
+	</main>
 </template>
 
 <style scoped>
-	.logo-wrapper {
+	.login-box {
+		background-color: #221c2b;
+		padding: 40px;
+		border-radius: 8px;
+		border: 1px solid #becfe4;
+		box-shadow: 0 4px 20px rgba(0, 0, 0, 0.4);
+		width: 100%;
+		max-width: 400px;
+	}
+
+	.form-title {
+		text-align: center;
+		font-size: 1.2rem;
+		color: #E4E4E4;
+		margin-bottom: 24px;
+	}
+
+	.input-group {
 		display: flex;
-		justify-content: center;
-		margin: 50px;
+		flex-direction: column;
+		gap: 8px;
+		margin-bottom: 16px;
+	}
+
+	input {
+		background-color: #0e0915;
+		border: 1px solid #221c2b;
+		color: #E4E4E4;
+		padding: 10px;
+		border-radius: 4px;
+	}
+
+	button {
+		background-color: #bd3fd8;
+		color: #0e0915;
+		border: none;
+		padding: 12px;
+		border-radius: 4px;
+		font-weight: bold;
+		cursor: pointer;
+		width: 100%;
 	}
 </style>
