62e1b6ece5d9dfc3cb4af14d4ffcee12cd906248
Kamillaaks <kammi.ar@hotmail.com>
2026-05-22 12:10:00 +0200

    Add Pinia board store with mock project data

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import {useBoardStore} from '~/stores/boardStore'
+
+const boardStore = useBoardStore()
+</script>
+
 <template>
-    
+    <div>
+        <h1>Dashboard</h1>
+
+        <div
+            v-for="project in boardStore.projects"
+            :key="project.id"
+        >
+            <h2>{{ project.name }}</h2>
+        </div>
+    </div>
 </template>
--- app/pages/project/[id].vue
+++ app/pages/project/[id].vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+import {useRoute} from 'vue-router'
+import { useBoardStore } from '~/stores/boardStore';
+
+const route = useRoute()
+const boardStore = useBoardStore()
+
+const projectId = Number(route.params.id)
+
+const project = boardStore.projects.find(
+    (p) => p.id === projectId
+)
+</script>
+
 <template>
-    
+    <div v-if="project">
+        <h1>{{ project.name }}</h1>
+
+        <h2>To do</h2>
+
+        <div
+            v-for="task in project.todo"
+            :key="task.id"
+        >
+            {{ task.title }}
+        </div>
+
+        <h2>Done</h2>
+
+        <div
+            v-for="task in project.done"
+            :key="task.id"
+        >
+            {{ task.title }}
+        </div>
+    </div>
 </template>
